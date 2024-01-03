c188958c21a6a84ca7abe60211ebc7c5c4ebc9f2
Aaron Roth <rotha@cip.ifi.lmu.de>
2024-01-03 17:35:00 +0100

    added mutation and recombine, also initiated Points in Constructor

--- src/ai/genetic/AiAgent.java
+++ src/ai/genetic/AiAgent.java
@@ -1,4 +1,7 @@
 package ai.genetic;
+
+import org.apache.commons.math3.distribution.NormalDistribution;
+
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -7,7 +10,7 @@ public class AiAgent implements Comparable<AiAgent> {
     AtomicInteger points;
 
     public AiAgent(int weightSize) {
-        this.points=new AtomicInteger();
+        this.points = new AtomicInteger();
 
         Random rnd = new Random();
         this.weights = new int[weightSize];
@@ -17,33 +20,102 @@ public class AiAgent implements Comparable<AiAgent> {
         }
     }
 
-    public void resetPoints(){
+    public AiAgent(int[] weights) {
+        this.weights = weights;
+        this.points=new AtomicInteger();
+    }
+
+    public static AiAgent mutate(AiAgent agent, NormalDistribution distribution) {
+        int[] newWeights = new int[agent.weights.length];
+        for (int i = 0; i < newWeights.length; i++) {
+            newWeights[i] = agent.weights[i] + (int) (distribution.sample());
+        }
+        return new AiAgent(newWeights);
+    }
+
+    public static AiAgent recombine(AiAgent mother, AiAgent father, int crossoverPercentage, NormalDistribution distribution) {
+        int[] childWeights = new int[mother.weights.length];
+        Random random = new Random();
+        int[] chromosomEndpoints = {1, ((mother.weights.length - 1) / 2) + 1, mother.weights.length};
+        int[] chromosomeSumsMother = getAbsolutSplitArraySum(chromosomEndpoints, mother.weights);
+        int[] chromosomeSumsFather = getAbsolutSplitArraySum(chromosomEndpoints, father.weights);
+
+        int gene = 0;
+        for (int currentChromosom = 0; currentChromosom < chromosomEndpoints.length; currentChromosom++) {
+            int chromosomEnd = chromosomEndpoints[currentChromosom];
+
+            int[] getsToInherent = mother.weights;
+            int[] notInherited = father.weights;
+
+            int inheritSum = chromosomeSumsMother[currentChromosom];
+            int notInheritSum = chromosomeSumsFather[currentChromosom];
+            if (random.nextBoolean()) {
+                getsToInherent = father.weights;
+                notInherited = mother.weights;
+                notInheritSum = chromosomeSumsMother[currentChromosom];
+                inheritSum = chromosomeSumsFather[currentChromosom];
+            }
+            while (gene < chromosomEnd) {
+                if (random.nextInt(100) < crossoverPercentage) {
+                    childWeights[gene] = getRecombinedScaledWeight(notInherited[gene], getsToInherent[gene], notInheritSum, inheritSum);
+                }else {
+                    childWeights[gene]=getsToInherent[gene];
+                }
+                //mutate
+                childWeights[gene]+=(int) (distribution.sample());
+                gene++;
+            }
+
+        }
+        return new AiAgent(childWeights);
+    }
+
+    public static int getRecombinedScaledWeight(int weightToScale, int weightToBeReplaced, int sumOfToScale, int sumOfToBeReplaced) {
+        int absWeightToScale=Math.abs(weightToScale);
+        if (sumOfToScale==absWeightToScale){
+            return weightToScale;
+        }
+        int absWeightToBeReplaced=Math.abs(weightToBeReplaced);
+        return (int) (weightToScale * (sumOfToBeReplaced - absWeightToBeReplaced) / (1.0 * sumOfToScale - absWeightToScale));
+    }
+
+    public static int[] getAbsolutSplitArraySum(int[] endpointsArray, int[] toSum) {
+        int[] sums = new int[endpointsArray.length];
+        int endPointIndex = 0;
+        for (int i = 0; i < toSum.length; i++) {
+            while (endpointsArray[endPointIndex] <= i) {
+                endPointIndex++;
+            }
+            sums[endPointIndex] += Math.abs(toSum[i]);
+        }
+
+        return sums;
+    }
+
+    public void resetPoints() {
         this.points.set(0);
     }
 
-    public AiAgent(int[] weights){
-        this.weights=weights;
-    }
-
-    public void addWin(){
+    public void addWin() {
         this.points.getAndAdd(3);
     }
-    public void addDraw(){
+
+    public void addDraw() {
         this.points.getAndAdd(1);
     }
 
     @Override
     public int compareTo(AiAgent o) {
-        return Integer.compare(this.points.get(),o.points.get());
+        return Integer.compare(this.points.get(), o.points.get());
     }
 
     @Override
-    public String toString(){
-        StringBuilder sb=new StringBuilder();
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
         for (int weight : weights) {
             sb.append(weight);
             sb.append("\t");
         }
-        return sb.substring(0,sb.length()-2);
+        return sb.substring(0, sb.length() - 2);
     }
 }
